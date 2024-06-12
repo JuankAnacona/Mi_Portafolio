7128e8881a4c72c1500c222e8e5ee0f01d967023
JuanKperro <juan.palacio3@educa.madrid.org>
2024-06-12 03:03:56 +0200

    Update portfolio

--- src/componentes/Layout/Nav.js
+++ src/componentes/Layout/Nav.js
@@ -30,8 +30,9 @@ function Nav() {
 
                         </ul>
                     </div>
-
-                    <button
+                    {
+                        /*
+                        <button
                         className="relative ml-auto h-6 max-h-[40px] w-6 max-w-[40px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-inherit transition-all hover:bg-transparent focus:bg-transparent active:bg-transparent disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:hidden"
                         type="button">
                         <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
@@ -41,6 +42,8 @@ function Nav() {
                             </svg>
                         </span>
                     </button>
+                        */ 
+                    }
                 </div>
             </div>
         </nav>
